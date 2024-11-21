fb6688f83cd699c63d89ad6df1612b98cfc9a038
Vineet Kumar <kumarvineet4893@gmail.com>
2024-11-21 22:08:10 +0530

    getQuizType api updated

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -6,7 +6,7 @@ export const getQuizType = async (req, res) => {
   try {
     const quiz = await Quiz.findOne({ type });
     if (!quiz) return res.status(404).json({ message: "Quiz not found" });
-    res.status(200).json({type:quiz.questions});
+    res.status(200).json(quiz.questions);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
